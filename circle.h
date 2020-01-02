42455a91b49541cacbf766532465962b66b8a0b3
Robert Leiner <leiner-robert@web.de>
2020-01-02 15:11:37 +0100

    circleSector polygon and circle changes

--- circle.h
+++ circle.h
@@ -8,9 +8,25 @@ class Circle : public Surface {
 	public:
 		//Contructors
 		Circle(Point center, int radius = 1);
+
+		//Getter
+		Point getCenter() { return center_m; }
+		int getRadius() { return radius_m; }
+
+		//Setter
+		void setCenter(int x, int y);
+		void setCenter(Point center);
+		void setRadius(int radius);
+
+		//Destructor
+		~Circle();
+
 	private:
 		Point center_m;
 		int radius_m;
+
+		
+
 };
 
 #endif
---
+++ circleSector.h
@@ -0,0 +1,35 @@
+#ifndef CIRCLE_SECTOR_H
+#define CIRCLE_SECTOR_H
+
+#include "surface.h"
+#include "point.h"
+
+class circleSector : public Surface {
+	public:
+		//Constructors
+		circleSector(Point center, int radius, double aperture, Point opening_point);
+
+		//Getter
+		Point getCenter() { return center_m; }
+		int getRadius() { return radius_m; }
+		double getAperture() { return aperture_m; }
+		Point getOpeningPoint() { return opening_point_m; }
+
+		//Setter
+		void setCenter();
+		void setRadius();
+		void setAperture();
+		void setOpeningPoint();
+
+		//Destructor
+		~circleSector();
+
+	private:
+		Point center_m;
+		int radius_m;
+		double aperture_m;
+		Point opening_point_m;
+
+};
+
+#endif
